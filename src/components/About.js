a86ef32ea951ab7b4dc64050e67df6acdf2234f6
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-09-09 18:16:02 +0100

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+        <h3>links:</h3>
+      <a href={props.linkedin}>Linkedin</a>
+      <a href={props.github}>Github</a>
+      
     </div>
   );
 }
